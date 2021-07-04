ea68f66bff4f53aa55a6582856c6e16da8b9efe6
swakabenehack03 <86813748+swakabenehack03@users.noreply.github.com>
2021-07-04 14:43:25 +0900

    Update in_thread.dart

--- lib/in_thread.dart
+++ lib/in_thread.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class InThreadPage extends StatelessWidget {
+  InThreadPage(this.name, this.category, this.theme, this.qustion);
+  String name, category, theme, qustion;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +12,64 @@ class InThreadPage extends StatelessWidget {
         ),
       ),
       body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('user:' + this.name, style: TextStyle(fontSize: 24)),
+            Text('category:' + this.category, style: TextStyle(fontSize: 24)),
+            Text('theme:' + this.theme, style: TextStyle(fontSize: 24)),
+            Text('question:' + this.qustion, style: TextStyle(fontSize: 24)),
+            /*Text('ようこそ！！', style: TextStyle(fontSize: 36)),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "テキストボックス",
+                hintText: "名前を入力しよう",
+              ),
+            ),
+            */
+            new Container(
+                width: 200.0,
+                height: 60.0,
+                child: new TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "username",
+                      hintText: "Please input name.",
+                    ),
+                    style: new TextStyle(
+                        fontSize: 15.0, height: 2.0, color: Colors.black))),
+            new Container(
+                width: 200.0,
+                height: 60.0,
+                child: new TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "reply",
+                      hintText: "Please input reply.",
+                    ),
+                    style: new TextStyle(
+                        fontSize: 15.0, height: 2.0, color: Colors.black))),
+            RaisedButton(
+              child: Text('投稿'),
+              onPressed: () {
+// -*--*--*--*--*- Navigator -*--*--*--*--*
+// -*--*--*--*--*- Navigator -*--*--*--*--*-
+              },
+            ),
+            RaisedButton(
+              child: Text('スレッド一覧に戻る'),
+              onPressed: () {
+// -*--*--*--*--*- Navigator -*--*--*--*--*-
+                Navigator.pop(context);
+// -*--*--*--*--*- Navigator -*--*--*--*--*-
+              },
+            ),
+          ],
+        ),
+      ),
+      /*
+      body: Center(
         child: RaisedButton(
           child: Text('スレッド一覧に戻る'),
           onPressed: () {
@@ -18,7 +78,7 @@ class InThreadPage extends StatelessWidget {
 // -*--*--*--*--*- Navigator -*--*--*--*--*-
           },
         ),
-      ),
+      ),*/
     );
   }
 }
