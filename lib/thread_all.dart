dbcad6eafcf162ea32e3df7312ca8337728ba23a
swakabenehack03 <86813748+swakabenehack03@users.noreply.github.com>
2021-07-04 13:32:24 +0900

    Update thread_all.dart

--- lib/thread_all.dart
+++ lib/thread_all.dart
@@ -18,15 +18,20 @@ class NextPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FloatingActionButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => CreateThreadPage()));
-                },
-                backgroundColor: Colors.blue,
-                child: Icon(Icons.add)),
+            //floatingActionbutton:Container(margin:EdgeInsets.only(buttom:50),child
+            Container(
+              margin: EdgeInsets.only(bottom: 0.0),
+              child: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CreateThreadPage()));
+                  },
+                  backgroundColor: Colors.blue,
+                  child: Icon(Icons.add)),
+            ),
+
             Text('ようこそ' + this.name + 'さん', style: TextStyle(fontSize: 24)),
             TextButton(
               onPressed: () {
